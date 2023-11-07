0c32388bf42a3141af4a950c8d46899ea5b8d428
alsrudrl1220 <alsrudrl1220@naver.com>
2023-11-07 22:19:04 +0900

    study week 2,3 added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication { //항상 프로그램의 최상단에 위치해있어야함.
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
